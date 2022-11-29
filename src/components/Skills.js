f463e59a99d8023ea0a6aa19ca51df99f734b5ba
jdbm69 <jhonnybracho2012@gmail.com>
2022-11-29 13:03:57 -0400

    casmbios tecnologia

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,4 @@
 import React, { useState } from "react"; 
-import bootstrap from "../resources/bootstrap.png";
-import css from "../resources/css.png";
-import html from "../resources/html.png";
-import js from "../resources/js.png";
-import photoshop from "../resources/photoshop.png";
-import python from "../resources/python.png";
-import react from "../resources/react.png";
-import sass from "../resources/sass.png";
 
 const Skills = () => {
 
@@ -25,9 +17,8 @@ const Skills = () => {
     return (
         <div className="container_skills" data-aos="fade-down" data-aos-delay="1200" data-aos-duration="1000">
             <div className="skill">
-                <div className="icono">
-                    <img src={html} alt="ico" className={barra ? 'imagen-active-uno' : 'imagen'}/>
-                    <h2>HTML</h2>
+                <div className="nombre">
+                    <h4>React.js</h4>
                 </div>
                 <div className="barras">
                     <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
@@ -35,59 +26,17 @@ const Skills = () => {
                 <p>90%</p>
             </div>
             <div className="skill">
-                <div className="icono">
-                    <img src={css} alt="ico" className={barra ? 'imagen-active-dos' : 'imagen'}/>
-                    <h2>CSS</h2>
+                <div className="nombre">
+                    <h4>React Native</h4>
                 </div>
                 <div className="barras">
-                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
+                    <div className="barra" id={barra ? 'cincuenta-active' : 'cincuenta'}></div>
                 </div>
-                <p>90%</p>
+                <p>50%</p>
             </div>
             <div className="skill">
-                <div className="icono">
-                    <img src={js} alt="ico" className={barra ? 'imagen-active-uno' : 'imagen'}/>
-                    <h2>Javascript</h2>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
-                </div>
-                <p>90%</p>
-            </div>
-            <div className="skill">
-                <div className="icono">
-                    <img src={react} alt="ico" className={barra ? 'imagen-active-dos' : 'imagen'}/>
-                    <h2>React</h2>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'ochenta-active' : 'ochenta'}></div>
-                </div>
-                <p>80%</p>
-            </div>
-            <div className="skill">
-                <div className="icono">
-                    <img src={sass} alt="ico" className={barra ? 'imagen-active-uno' : 'imagen'}/>
-                    <h2>Sass</h2>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'ochenta-active' : 'ochenta'}></div>
-                </div>
-                <p>80%</p>
-            </div>
-            <div className="skill">
-                <div className="icono">
-                    <img src={bootstrap} alt="ico" className={barra ? 'imagen-active-dos' : 'imagen'}/>
-                    <h2>Bootstrap</h2>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'setenta-active' : 'setenta'}></div>
-                </div>
-                <p>70%</p>
-            </div>
-            <div className="skill">
-                <div className="icono">
-                    <img src={python} alt="ico" className={barra ? 'imagen-active-uno' : 'imagen'}/>
-                    <h2>Python</h2>
+                <div className="nombre">
+                    <h4>Next.js</h4>
                 </div>
                 <div className="barras">
                     <div className="barra" id={barra ? 'sesenta-active' : 'sesenta'}></div>
@@ -95,9 +44,89 @@ const Skills = () => {
                 <p>60%</p>
             </div>
             <div className="skill">
-                <div className="icono">
-                    <img src={photoshop} alt="ico" className={barra ? 'imagen-active-dos' : 'imagen'}/>
-                    <h2>Photoshop</h2>
+                <div className="nombre">
+                    <h4>HTML</h4>
+                </div>
+                <div className="barras">
+                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
+                </div>
+                <p>90%</p>
+            </div>
+            <div className="skill">
+                <div className="nombre">
+                    <h4>CSS</h4>
+                </div>
+                <div className="barras">
+                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
+                </div>
+                <p>90%</p>
+            </div>
+            <div className="skill">
+                <div className="nombre">
+                    <h4>Javascript</h4>
+                </div>
+                <div className="barras">
+                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
+                </div>
+                <p>90%</p>
+            </div>
+            <div className="skill">
+                <div className="nombre">
+                    <h4>TypeScript</h4>
+                </div>
+                <div className="barras">
+                    <div className="barra" id={barra ? 'setenta-active' : 'setenta'}></div>
+                </div>
+                <p>70%</p>
+            </div>
+            <div className="skill">
+                <div className="nombre">
+                    <h4>SASS</h4>
+                </div>
+                <div className="barras">
+                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
+                </div>
+                <p>90%</p>
+            </div>
+            <div className="skill">
+                <div className="nombre">
+                    <h4>Bootstrap</h4>
+                </div>
+                <div className="barras">
+                    <div className="barra" id={barra ? 'ochenta-active' : 'ochenta'}></div>
+                </div>
+                <p>80%</p>
+            </div>
+            <div className="skill">
+                <div className="nombre">
+                    <h4>Python</h4>
+                </div>
+                <div className="barras">
+                    <div className="barra" id={barra ? 'setenta-active' : 'setenta'}></div>
+                </div>
+                <p>70%</p>
+            </div>
+            <div className="skill">
+                <div className="nombre">
+                    <h4>Git & GitHub</h4>
+                </div>
+                <div className="barras">
+                    <div className="barra" id={barra ? 'ochenta-active' : 'ochenta'}></div>
+                </div>
+                <p>80%</p>
+            </div>
+            <div className="skill">
+                <div className="nombre">
+                    <h4>Photoshop</h4>
+                </div>
+                <div className="barras">
+                    <div className="barra" id={barra ? 'cincuenta-active' : 'cincuenta'}></div>
+                </div>
+                <p>50%</p>
+            </div>
+            <div className="skill">
+                <div className="nombre">
+                    <h4>Figma</h4>
                 </div>
                 <div className="barras">
                     <div className="barra" id={barra ? 'cincuenta-active' : 'cincuenta'}></div>
