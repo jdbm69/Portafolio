7d62f4fa7a717b41eddbc90652aedce731129579
jdbm69 <jhonnybracho2012@gmail.com>
2022-12-01 10:16:14 -0400

    bucle en skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,127 +12,102 @@ const Skills = () => {
         }
     };
 
+    const skills = [
+        {
+            nombre: 'React.js',
+            porcentaje: '90%',
+            barra: 'noventa',
+            barraActive: 'noventa-active'
+        },
+        {
+            nombre: 'React Native',
+            porcentaje: '50%',
+            barra: 'cincuenta',
+            barraActive: 'cincuenta-active'
+        },
+        {
+            nombre: 'Next.js',
+            porcentaje: '60%',
+            barra: 'sesenta',
+            barraActive: 'sesenta-active'
+        },
+        {
+            nombre: 'HTML',
+            porcentaje: '90%',
+            barra: 'noventa',
+            barraActive: 'noventa-active'
+        },
+        {
+            nombre: 'CSS',
+            porcentaje: '90%',
+            barra: 'noventa',
+            barraActive: 'noventa-active'
+        },
+        {
+            nombre: 'JavaScript',
+            porcentaje: '90%',
+            barra: 'noventa',
+            barraActive: 'noventa-active'
+        },
+        {
+            nombre: 'TypeScript',
+            porcentaje: '70%',
+            barra: 'setenta',
+            barraActive: 'setenta-active'
+        },
+        {
+            nombre: 'SASS',
+            porcentaje: '90%',
+            barra: 'noventa',
+            barraActive: 'noventa-active'
+        },
+        {
+            nombre: 'Bootstrap',
+            porcentaje: '80%',
+            barra: 'ochenta',
+            barraActive: 'ochenta-active'
+        },
+        {
+            nombre: 'Python',
+            porcentaje: '70%',
+            barra: 'setenta',
+            barraActive: 'setenta-active'
+        },
+        {
+            nombre: 'Git & GitHub',
+            porcentaje: '80%',
+            barra: 'ochenta',
+            barraActive: 'ochenta-active'
+        },
+        {
+            nombre: 'Photoshop',
+            porcentaje: '50%',
+            barra: 'cincuenta',
+            barraActive: 'cincuenta-active'
+        },
+        {
+            nombre: 'Figma',
+            porcentaje: '50%',
+            barra: 'cincuenta',
+            barraActive: 'cincuenta-active'
+        },
+    ];
+
     window.addEventListener('scroll', rellenarBarra);
 
     return (
         <div className="container_skills" data-aos="fade-down" data-aos-delay="1200" data-aos-duration="1000">
-            <div className="skill">
-                <div className="nombre">
-                    <h4>React.js</h4>
+            {skills.map(skill =>(
+                <div className="skill">
+                    <div className="nombre">
+                        <h4>{skill.nombre}</h4>
+                    </div>
+                    <div className="barras">
+                        <div className="barra" id={barra ? skill.barraActive : skill.barra}></div>
+                    </div>
+                    <p>{skill.porcentaje}</p>
                 </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
-                </div>
-                <p>90%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>React Native</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'cincuenta-active' : 'cincuenta'}></div>
-                </div>
-                <p>50%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>Next.js</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'sesenta-active' : 'sesenta'}></div>
-                </div>
-                <p>60%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>HTML</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
-                </div>
-                <p>90%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>CSS</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
-                </div>
-                <p>90%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>Javascript</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
-                </div>
-                <p>90%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>TypeScript</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'setenta-active' : 'setenta'}></div>
-                </div>
-                <p>70%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>SASS</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'noventa-active' : 'noventa'}></div>
-                </div>
-                <p>90%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>Bootstrap</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'ochenta-active' : 'ochenta'}></div>
-                </div>
-                <p>80%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>Python</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'setenta-active' : 'setenta'}></div>
-                </div>
-                <p>70%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>Git & GitHub</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'ochenta-active' : 'ochenta'}></div>
-                </div>
-                <p>80%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>Photoshop</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'cincuenta-active' : 'cincuenta'}></div>
-                </div>
-                <p>50%</p>
-            </div>
-            <div className="skill">
-                <div className="nombre">
-                    <h4>Figma</h4>
-                </div>
-                <div className="barras">
-                    <div className="barra" id={barra ? 'cincuenta-active' : 'cincuenta'}></div>
-                </div>
-                <p>50%</p>
-            </div>
+            ))}
         </div>
     )
 };
